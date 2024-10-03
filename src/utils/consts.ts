5e44c59d6bb660eae30a09986277f1427521a6ad
Jorge Sánchez <61122520+JorgeSanchezL@users.noreply.github.com>
2024-10-03 17:20:16 +0200

    Update consts.ts

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -15,7 +15,7 @@ export enum Round {
 }
 
 export const WORDS = [
-    "Jhonny Depp", "Antonio Banderas", "Brad Pitt", "Adam Sandler", "Harrison Ford", "Jet Li", "Al Pacino", "Scarface", "Frozen", "Hércules", "Aladdin", "El Dictador", "El Padrino", "Manzana", "Pizza", "Pato", "Montaña", "Mesa", "Montaña", "Pueblo", "Carretera", "Natos y Waor", "Bad Bunny", "AC/DC", "Nirvana", "Queen", "Bob Marley", "Juan Carlos I", "Pedro Sánchez", "Barco", "Espina", "Rosa", "Papel", "Avión", "Burguer King", "Pictionary", "Tenis", "Dani Parejo", "Hospital", "Jurassic Park"
+    "Jhonny Depp", "Antonio Banderas", "Brad Pitt", "Adam Sandler", "Harrison Ford", "Jet Li", "Al Pacino", "Scarface", "Frozen", "Hércules", "Aladdin", "El Dictador", "El Padrino", "Manzana", "Pizza", "Pato", "Montaña", "Mesa", "Montaña", "Pueblo", "Carretera", "Natos y Waor", "Bad Bunny", "AC/DC", "Nirvana", "Queen", "Bob Marley", "Juan Carlos I", "Pedro Sánchez", "Barco", "Espina", "Rosa", "Papel", "Avión", "Burguer King", "Pictionary", "Tenis", "Dani Parejo", "Manolo Lama", "Hospital", "Jurassic Park"
 ]
 
 /* export const WORDS = [
